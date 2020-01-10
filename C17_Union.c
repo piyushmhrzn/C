ac087119f3f51c288a51726ea40aed64d13db8e4
Piyush Maharjan <pmaharjanrocks@gmail.com>
2020-01-10 19:48:02 +0545

    Union Completed

--- C17_Union.c
+++ C17_Union.c
@@ -15,7 +15,39 @@ Description: C programming Essentials practices from Linda.com
 
 int main()
 {
+    /*
+    In this imaginary protocol, it has been specified that, based on the "message type", the following location
+    in the header will either be a request number, or a four character code, but not both. In short, unions allow
+    for the same storage location to represent more than one data type, where it is guaranteed that you will only
+    want to store one of the types of data at any one time.
 
+    struct packetHeader {
+               int sourceAddress;
+               int destinationAddress;
+               int messageType;
+               union request {
+                       char fourCharacter[4];
+                       int requestNumber;
+           };
+        };
+    */
+    union symbol{
+        int number;
+        char letter;
+    };
+
+    union symbol value;
+
+    value.number = 25;
+    value.letter = 'm';           //m has replaced 25;
+
+    union symbol *ptr;
+
+    ptr = &value;                                            //Storing address of value
+    char myLetter = ptr -> letter;         //Accessing Letter Field
+
+    printf("Letter after Union: %c\n",myLetter);
+    printf("Letter after Union: %d\n",myLetter);             //ASCII Value
 
     return EXIT_SUCCESS;
 }
